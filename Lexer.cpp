f063e2d13b65c1358dbf487eb18bb06fb514c07a
Bryn McKerracher <79728898+BrynMcKerracher@users.noreply.github.com>
2024-08-21 08:49:36 +1200

    Add submodule dependency, CMakeLists.txt, and Lexer skeleton with test suite

---
+++ Lexer.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file Lexer.cpp
+ * @author brynm
+ */
+#include "Lexer.h"
+
+namespace Noble {
+namespace Compiler {
+} // Compiler
+} // Noble
---
+++ Lexer.h
@@ -0,0 +1,12 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+namespace Noble::Compiler {
+    class Lexer {
+    public:
+
+    protected:
+    };
+} // Noble::Compiler
+
+#endif //LEXER_H
