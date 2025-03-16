e382299a476e94a66767098b7a00af3e3461e0ac
bryn <brynmckerracher@gmail.com>
2025-03-16 18:59:05 +1300

    Add error message if unable to open any files designation for compilation

--- main.cpp
+++ main.cpp
@@ -6,17 +6,24 @@ using namespace Noble::Compiler;
 
 /**
  * @param fileName The name of the file whose contents we want to read
+ * @param fileBuffer Reference to a string where the file contents are stored
  * @return The contents of the file, unformatted, as a std::string
  */
-std::string ReadFile(const std::string& fileName)
+bool ReadFile(const std::string& fileName, std::string& fileBuffer)
 {
     std::ifstream file(fileName);
+    if (!file.is_open())
+    {
+        std::cout << "Failed to open file '" << fileName << "'\n";
+        return false;
+    }
     file.seekg(0, std::ios::end);
     const long size = file.tellg();
     std::string buffer(size, ' ');
     file.seekg(0);
     file.read(&buffer[0], size);
-    return buffer;
+    fileBuffer = buffer;
+    return true;
 }
 
 int main(const int argc, char** argv)
@@ -36,7 +43,11 @@ int main(const int argc, char** argv)
     //Assume each arg is the name of a file to be compiled
     for (int i = 1; i < argc; ++i)
     {
-        const std::string fileContents = ReadFile(argv[i]);
+        std::string fileContents;
+        if (!ReadFile(argv[i], fileContents))
+        {
+            return -1;
+        }
         const std::string fileTitle = argv[i];
         const std::string fileName = fileTitle.substr(0, fileTitle.find_first_of('.'));
 
